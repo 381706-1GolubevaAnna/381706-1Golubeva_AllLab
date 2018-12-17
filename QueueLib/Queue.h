786b40bf697cfcc1b8a426948558523c47b2a91e
381706-1GolubevaAnna <43142260+381706-1GolubevaAnna@users.noreply.github.com>
2018-12-17 18:33:44 +0300

    Add files via upload

---
+++ QueueLib/Queue.h
@@ -0,0 +1,80 @@
+#pragma once
+#include "../StackLib/Stack.h"
+
+template <class T>
+class TQueue : public TStack <T> 
+{
+protected:
+	int start;
+	int count;
+public:
+	TQueue(int n = 0);
+	TQueue(TQueue <T> &q);
+	virtual ~TQueue();
+	void Put(T a);
+	T Get();
+	bool IsFull();
+	bool IsEmpty();
+};
+
+template <class T>
+TQueue<T>::TQueue(int n) : TStack<T>(n) 
+{
+	start = 0;
+	count = 0;
+}
+
+template <class T>
+
+TQueue<T>::TQueue(TQueue<T> &q) : TStack<T>(q)
+{
+	start = q.start;
+	count = q.count;
+}
+
+template <class T>
+TQueue<T>::~TQueue() {}
+
+template <class T>
+void TQueue<T>::Put(T a) 
+{
+	if (IsFull())
+		throw TException("Error. Queue is full.");
+	else 
+	{
+		TStack<T>::mas[start] = a;
+		start = (start + 1) % TStack<T>::size;
+		count++;
+	}
+}
+
+template <class T>
+T TQueue<T>::Get() {
+	if (IsEmpty())
+		throw TException("Error.Queue is empty");
+	else 
+	{
+		T temp = TStack<T>::mas[TStack<T>::top];
+		TStack<T>::top = (TStack<T>::top + 1) % TStack<T>::size;
+		count--;
+		return temp;
+	}
+}
+
+template <class T>
+bool TQueue<T>::IsFull() 
+{
+	if (count == TStack<T>::size)
+		return true;
+	else
+		return false;
+}
+
+template <class T>
+bool TQueue<T>::IsEmpty()
+{
+	if (count == 0)
+		return true;
+	else
+		return false;
+}
