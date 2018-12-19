e1ab08d97a9be17a81aab4a1c9ec5612a0bd93a4
381706-1GolubevaAnna <43142260+381706-1GolubevaAnna@users.noreply.github.com>
2018-12-19 14:17:25 +0300

    Update main.cpp

--- Queue/main.cpp
+++ Queue/main.cpp
@@ -1,21 +1,33 @@
-
 #include <iostream>
 #include "../QueueLib/Queue.h"
-
+#include "Exception.h"
+#include <locale.h>
 using namespace std;
 
 int main()
-{   
+{   setlocale(LC_ALL, "Russian");
 	try
 	{
-	TQueue<int> queue(5);
-	queue.Put(1);
-	queue.Put(2);
-	queue.Put(3);
-	int temp;
-	temp = queue.Get();
-	cout << temp <<endl;
+	int n;
+	cout << "Введите количество элементов в очереди\n"
+	cin >> n;
+	TQueue<int> queue(n);
+	int elem;
+	cout << "Введите элементы стека\n";
+	for (int i = 0; i < n; i++)
+	{
+		cin >> elem;
+		queue.Put(elem);
+	}
+	cout << "\nВывод очереди на экран\n";
+	queue.PrintQueue();
+	cout << "\Скопировать очередь\n";
 	TQueue <int> queue1(queue);
+	queue1.PrintQueue();
+	cout << "\nЗабрать элемент из очереди\n";
+	queue.Get();
+	cout << "\nВывод очереди на экран\n";
+	queue.PrintQueue();
 	}
   catch (TException exception)
   {
