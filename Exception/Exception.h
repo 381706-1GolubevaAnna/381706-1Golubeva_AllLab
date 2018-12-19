a1e30c247256251d0e5cc1efd12c7d92d4ce40d0
381706-1GolubevaAnna <43142260+381706-1GolubevaAnna@users.noreply.github.com>
2018-12-19 17:49:55 +0300

    Update Exception.h

--- Exception/Exception.h
+++ Exception/Exception.h
@@ -6,16 +6,16 @@ using namespace std;
 
 class TException
 {
-private:
-	string message;
-public:
-	TException(string _message);
-	void Print();
+  private:
+  string message;
+  public:
+  TException(string _message);
+  void Print();
 };
 
 TException::TException(string _message) : message(_message) {}
 
 void TException::Print()
 {
-	cout << "\nWarning! \nMessage: " << message << endl;
+  cout << "\nWarning! \nMessage: " << message << endl;
 }
