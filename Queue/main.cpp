b67c3a8d995ee0ca0df738cc5f517ee753ebc058
381706-1GolubevaAnna <43142260+381706-1GolubevaAnna@users.noreply.github.com>
2018-12-19 14:30:37 +0300

    Update main.cpp

--- Queue/main.cpp
+++ Queue/main.cpp
@@ -9,26 +9,26 @@ int main()
   setlocale(LC_ALL, "Russian");
 	try
 	{
-	int n;
-	cout << "Введите количество элементов в очереди\n"
-	cin >> n;
-	TQueue<int> queue(n);
-	int elem;
-	cout << "Введите элементы очереди\n";
-	for (int i = 0; i < n; i++)
-	{
-		cin >> elem;
-		queue.Put(elem);
-	}
-	cout << "\nВывод очереди на экран\n";
-	queue.PrintQueue();
-	cout << "\Скопировать очередь\n";
-	TQueue <int> queue1(queue);
-	queue1.PrintQueue();
-	cout << "\nЗабрать элемент из очереди\n";
-	queue.Get();
-	cout << "\nВывод очереди на экран\n";
-	queue.PrintQueue();
+    int n;
+    cout << "Введите количество элементов в очереди\n"
+    cin >> n;
+    TQueue<int> queue(n);
+    int elem;
+    cout << "Введите элементы очереди\n";
+    for (int i = 0; i < n; i++)
+    {
+      cin >> elem;
+      queue.Put(elem);
+    }
+    cout << "\nВывод очереди на экран\n";
+    queue.PrintQueue();
+    cout << "\Скопировать очередь\n";
+    TQueue <int> queue1(queue);
+    queue1.PrintQueue();
+    cout << "\nЗабрать элемент из очереди\n";
+    queue.Get();
+    cout << "\nВывод очереди на экран\n";
+    queue.PrintQueue();
 	}
   catch (TException exception)
   {
