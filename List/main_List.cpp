a05d2c2e66fc2e1041ea9f3aff03fcc448e55d9f
381706-1GolubevaAnna <43142260+381706-1GolubevaAnna@users.noreply.github.com>
2019-02-08 22:04:02 +0300

    Update main_List.cpp

--- List/main_List.cpp
+++ List/main_List.cpp
@@ -3,23 +3,33 @@
 int main()
 {
 		TList<int> L;
-		int n1, n2;
+		int n1, n2, temp;
 		cout << "Put the elements in the beginning. Enter the number of elements: ";
 		cin >> n1;
+		cout << "Enter the elements: ";
 		for (int i = 1; i <= n1; i++)
-			L.PutBegin(i);
+		{
+			cin >> temp;
+			L.PutBegin(temp);
+		}
 		cout << "\nList: ";
 		L.Print();
 		cout << "\n Put the elements in the end. Enter the number of elements:";
 		cin >> n2;
+		cout << "Enter the elements: ";
 		for (int i = 1; i <= n2; i++)
-			L.PutEnd(i);
+		{
+			cin >> temp;
+			L.PutEnd(temp);
+		}
 		cout << "\nList\n";
 		L.Print();
-		cout << "\nGet the element from the beginning of the List ";
+		cout << "\nGet the element from the beginning of the List: ";
 		cout << L.GetBegin();
-		cout << "\nGet the element from the end of the List ";
+		cout << "\nGet the element from the end of the List: ";
 		cout<< L.GetEnd();
 		cout << "\nList:\n";
-		L.Print();
+		L.Print(); 
+		cout << endl;
+
 }
