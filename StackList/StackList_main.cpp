bb9fbd128142423ad545c7eb8502db871fb7d956
381706-1GolubevaAnna <43142260+381706-1GolubevaAnna@users.noreply.github.com>
2019-02-08 23:25:01 +0300

    Update StackList_main.cpp

--- StackList/StackList_main.cpp
+++ StackList/StackList_main.cpp
@@ -5,5 +5,21 @@ using namespace std;
 
 int main()
 {
-return 0;
+  cout <<  "\nEnter stack size: ";
+	int size,temp;
+	cin >> size;
+		TStackList<int> St(size);
+		cout << "Enter the elements: ";
+		for (int i = 1; i <= size; i++)
+		{
+			cin >> temp;
+			St.Put(temp);
+		}
+		cout << "\nStack :\n";
+		St.Print();
+		cout << endl;
+		cout << "\nGet element of the Stack: ";
+		cout << St.Get();
+		cout << endl;
+  return 0;
 }
