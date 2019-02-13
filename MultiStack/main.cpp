d4d8a34c1948861f2f7a52851130471f8b032445
381706-1GolubevaAnna <43142260+381706-1GolubevaAnna@users.noreply.github.com>
2019-02-13 22:06:59 +0300

    Update main.cpp

--- MultiStack/main.cpp
+++ MultiStack/main.cpp
@@ -6,14 +6,17 @@ using namespace std;
 
 int main()
 {
-	/*
-	TMStack <int> MStack(3, 12);
-	MStack.Set(2, 1);
-	MStack.Set(2, 2);
-	MStack.Set(2, 3);
-	cout << MStack.Get(2);
+	TMStack <int> MStack(5, 15);
+	MStack.Set(1, 1);
+	MStack.Set(1, 2);
+	MStack.Set(1, 3);
+	MStack.Set(1, 4);
 	cout << endl;
-    cout << MStack.Get(2);
-    */
+	cout << MStack.Get(1);
+	cout << endl;
+        cout << MStack.Get(1);
+	cout << endl;
+        cout << MStack.Get(1);
+	cout << endl;
 	return 0;
 }
