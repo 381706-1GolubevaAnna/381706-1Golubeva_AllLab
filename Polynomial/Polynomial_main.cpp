9c18e94ef8bec726a629577612cc9d3b4384b391
381706-1GolubevaAnna <43142260+381706-1GolubevaAnna@users.noreply.github.com>
2019-02-14 23:25:05 +0300

    Add files via upload

---
+++ Polynomial/Polynomial_main.cpp
@@ -0,0 +1,19 @@
+#include "Polynomial.h"
+
+int main()
+{
+
+	int mas[] = { 1,2,3 },
+    int nmas[] = { 7,3,4};
+
+	TMonom M1(3, mas, 2),  M2(3, nmas, 1);
+
+	cout << "\tMonomial 1: " << M1 << "\n";
+	cout << "\tMonomial 2: " << M2 << "\n";
+
+	TPolynom P(3);
+	P += M1;
+	P += M2;
+	cout << "\n\tPolynomial = M1 + M2 = " << P << "\n";
+	return 0;
+}
