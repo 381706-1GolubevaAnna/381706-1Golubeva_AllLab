852a0e92ade8a4c48491da1fe4d606d6ac08ec07
381706-1GolubevaAnna <43142260+381706-1GolubevaAnna@users.noreply.github.com>
2019-02-17 22:48:01 +0300

    Update Polish_main.cpp

--- Polish/Polish_main.cpp
+++ Polish/Polish_main.cpp
@@ -5,20 +5,12 @@ using namespace std;
 
 int main()
 {
-	
-	{
-		char a[] = "(-1*7+9/3)";
-		TString example(a);
-		TQueue<char> B;
-		cout << "\t" << example << "\n\n";
-		B = ConvertToPolish(example);
-
-		cout << "\t"; B.PrintQueue();
-		cout << " = " << Res(B) << "\n";
-
-	}
-
-	
-
-	return 0;
+    cout << "Enter math expression:";
+    TString str;
+    cin >> str;
+    cout << str;
+    TQueue<char> B;
+    B = ConvertToPolish(str);
+    cout << " = " << Rez(B) << endl;
+    return 0;
 }
