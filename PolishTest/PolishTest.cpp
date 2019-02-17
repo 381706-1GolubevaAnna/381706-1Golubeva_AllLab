442d3d8a2e88eecd015741a4f8f040b4cec6e7ad
381706-1GolubevaAnna <43142260+381706-1GolubevaAnna@users.noreply.github.com>
2019-02-17 21:26:07 +0300

    Update PolishTest.cpp

--- PolishTest/PolishTest.cpp
+++ PolishTest/PolishTest.cpp
@@ -22,14 +22,14 @@ TEST(Polish, can_check_is_operation)
 	EXPECT_EQ(IsOperation('('), true);
 	EXPECT_EQ(IsOperation('.'), false);
 }
-/*
+
 TEST(Polish, can_convert_to_polish)
 {
-	char s[] = '14+5';
+	char s[] = "14+5";
 	TString str(s);
 	TQueue<char> A;
-
-	ASSERT_NO_THROW(A = ConvertToPolish(str));
+        A = ConvertToPolish(str);
+	
 	EXPECT_EQ('{', A.Get());
 	EXPECT_EQ('14', A.Get());
 	EXPECT_EQ('}', A.Get());
@@ -38,4 +38,71 @@ TEST(Polish, can_convert_to_polish)
 	EXPECT_EQ('}', A.Get());
 	EXPECT_EQ('+', A.Get());
 }
-*/
+
+TEST(Polish, can_add)
+{
+  char s[] = "5+4";
+  TString str(s);
+  TQueue<char> B;
+  B = ConvertToPolish(str);
+  EXPECT_EQ(9, Res(B));
+}
+TEST(Polish, can_subtract) 
+{
+  char s[] = "10-5";
+  TString A(s);
+  TQueue<char> B;
+  B = ConvertToPolish(A);
+
+  EXPECT_EQ(5, Res(B));
+}
+
+TEST(Polish, can_multiply)
+{
+  char s[] = "5*4";
+  TString A(s);
+  TQueue<char> B;
+  B = ConvertToPolish(A);
+
+  EXPECT_EQ(20, Res(B));
+}
+
+TEST(Polish, throw_when_math_expression_have_is_an_unacceptable_symbol)
+{
+  char s[] = "5#6";
+  TString A(s);
+  TQueue<char> B;
+
+  ASSERT_ANY_THROW(B = ToPolishish(A));
+}
+
+TEST(Polish, throw_when_first_operation_is_something)
+{
+  char s[] = "*5+8";
+
+  TString A(s)
+  TQueue<char> B;
+
+  ASSERT_ANY_THROW(B = ConvertToPolish(A));
+}
+
+TEST(Polish, throw_when_kol_sk_wrong)
+{
+  char s[] = "-2+8)";
+  char s1[] = "(-2+8";
+
+  TString A(s), B(s1),
+  TQueue<char> C, D;
+
+  ASSERT_ANY_THROW(C = ConvertToPolish(A));
+  ASSERT_ANY_THROW(D = ConvertToPolish(B));
+}
+TEST(Polish, can_del)
+{
+  char s[] = "10/2";
+  TString A(s);
+  TQueue<char> B;
+  B = ConvertToPolish(A);
+
+  EXPECT_EQ(5, Res(B));
+}
