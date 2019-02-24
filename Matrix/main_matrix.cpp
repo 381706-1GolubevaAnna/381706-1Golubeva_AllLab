933ab51136aed844b9eb2c78a6939528c3bafea8
381706-1GolubevaAnna <43142260+381706-1GolubevaAnna@users.noreply.github.com>
2019-02-24 19:22:17 +0300

    Update main_matrix.cpp

--- Matrix/main_matrix.cpp
+++ Matrix/main_matrix.cpp
@@ -6,33 +6,15 @@ int main()
   try
   {
     TMatrix<int> A(5), B(2), res(2);
-    /*A[0][0] = 7;
+    A[0][0] = 7;
     A[0][1] = 2;
     A[1][0] = 3;
-    A[1][1] = 6;
-	  */
+   
     B[0][0] = 4;
     B[0][1] = 3;
-    B[1][1] = 3;  
     B[1][0] = 2;
  
-      for (int i = 0; i < 5; i++)
-	for (int j = 0; j < 5 - i; j++) 
-	{
-	  A[i][j] = (i + 1) * (j + 1);
-	 
-	}
-    cout << A[2];
-    cout<<endl;
-    cout << "A\n";
-    cout << A << endl;
-	  cout<<"B[0]="<<B[0]<<endl;
-	  cout<<"B[1]="<< B[1]<<endl;
-    cout << "B" <<endl;
-    cout << B << endl; 
-    cout<< "B[1][1] = "<< B[1][1]<<endl;
-  }
-  /*
+   
     res = A + B;
     cout << "A + B" << endl << res << endl;
 
@@ -45,7 +27,6 @@ int main()
     res = A / B;
     cout << "A / B" << endl << res << endl;
   }
-*/
  catch (TException exception)
   {
     exception.Print();
