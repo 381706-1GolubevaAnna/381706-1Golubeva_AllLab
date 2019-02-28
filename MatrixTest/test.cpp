84228f4d0414de784d38cba284a11b006bdb852b
381706-1GolubevaAnna <43142260+381706-1GolubevaAnna@users.noreply.github.com>
2019-02-28 19:11:30 +0300

    Update test.cpp

--- MatrixTest/test.cpp
+++ MatrixTest/test.cpp
@@ -32,96 +32,6 @@ TEST(Matrix, can_assign_matrix)
   ASSERT_TRUE(m1 == m2);
 }
 
-TEST(Matrix, can_add_matrices)
-{
-	TMatrix <int> m1(2), m2(2), m(2);
-	m1[0][0] = 1;
-	m1[0][1] = 2;
-	m1[1][0] = 4;
-	m2[0][0] = 5;
-	m2[0][1] = 6;
-	m2[1][0] = 8;
-	for (int i = 0; i < 2; i++)
-	{
-		for (int j = 0; j < 2; j++)
-			m[i][j] = m1[i][j] + m2[i][j];
-	}
-	ASSERT_TRUE(m == (m1 + m2));
-}
-
-TEST(Matrix, thorws_when_add_matrices_with_different_size)
-{
-	TMatrix <int> m1(1), m2(7);
-	ASSERT_ANY_THROW(m1 + m2);
-}
-
-TEST(Matrix, can_subtract_matrices)
-{
-	TMatrix <int> m1(2), m2(2), m(2);
-	m1[0][0] = 1;
-	m1[0][1] = 2;
-	m1[1][0] = 4;
-	m2[0][0] = 5;
-	m2[0][1] = 6;
-	m2[1][0] = 8;
-	for (int i = 0; i < 2; i++)
-	{
-		for (int j = 0; j < 2; j++)
-			m[i][j] = m1[i][j] - m2[i][j];
-	}
-	ASSERT_TRUE(m == (m1 - m2));
-}
-
-
-TEST(TMatrix, throws_when_subtract_matrices_with_different_size)
-{
-	TMatrix <int> m1(7);
-	TMatrix <int> m2(3);
-
-	ASSERT_ANY_THROW(m1 - m2);
-}
-
-TEST(TMatrix, can_multiply_matrices)
-{
-	TMatrix <int> m1(2), m2(2), m(2);
-
-	m1[0][0] = 1;
-	m1[0][1] = 2;
-	m1[1][0] = 3;
-	m2[0][0] = 5;
-	m2[0][1] = 4;
-	m2[1][0] = 3;
-
-	m[0][0] = 5;
-	m[0][1] = 10;
-	m[1][0] = 9;
-
-	ASSERT_TRUE(m == (m1 * m2));
-}
-
-TEST(Matrix, thorws_when_multiply_matrices_with_different_size)
-{
-	TMatrix <int> m1(7), m2(8);
-	ASSERT_ANY_THROW(m1 * m2);
-}
-
-TEST(TMatrix, can_divide_matrix)
-{
-	TMatrix<int> m1(2), m2(2), m(2);
-
-	m1[0][0] = 1;
-	m1[0][1] = 2;
-	m1[1][0] = 1;
-
-	m2[0][0] = 1;
-	m2[0][1] = 1;
-	m2[1][0] = 1;
-
-	m[0][0] = 1;
-	m[0][1] = 1;
-	m[1][0] = 1;
-	ASSERT_TRUE(m == (m1 / m2));
-}
 
 TEST(Matrix, equality_matrix_true)
 {
@@ -174,3 +84,86 @@ TEST(TMatrix, inequality_matrix_false)
 
 	ASSERT_FALSE(m1 != m2);
 }
+
+TEST(Matrix, can_add_matrices)
+{
+  TMatrix<int> m1(3);
+  TMatrix<int> m2(3);
+  TMatrix<int> m3(3);
+
+  m1[1][1] = 1;
+  m2[1][1] = 1;
+  m3[1][1] = 2;
+
+  EXPECT_EQ(m3, m1 + m2);
+}
+
+TEST(Matrix, thorws_when_add_matrices_with_different_size)
+{
+	TMatrix <int> m1(1), m2(7);
+	ASSERT_ANY_THROW(m1 + m2);
+}
+
+TEST(Matrix, can_subtract_matrices)
+{
+	TMatrix <int> m1(3);
+	TMatrix <int> m2(3);
+	TMatrix <int> m3(3);
+
+	m1[1][2] = 2;
+	m2[1][2] = 1;
+	m3 = m1 - m2;
+
+	EXPECT_EQ(m3[1][2], 1);
+}
+
+
+TEST(TMatrix, throws_when_subtract_matrices_with_different_size)
+{
+	TMatrix <int> m1(7);
+	TMatrix <int> m2(3);
+
+	ASSERT_ANY_THROW(m1 - m2);
+}
+
+TEST(TMatrix, can_multiply_matrices)
+{
+	TMatrix <int> m1(2), m2(2), m(2);
+
+	m1[0][0] = 1;
+	m1[0][1] = 2;
+	m1[1][0] = 3;
+	m2[0][0] = 5;
+	m2[0][1] = 4;
+	m2[1][0] = 3;
+	m[0][0] = 5;
+	m[0][1] = 10;
+	m[1][0] = 9;
+
+	ASSERT_TRUE(m == (m1 * m2));
+}
+
+TEST(Matrix, thorws_when_multiply_matrices_with_different_size)
+{
+	TMatrix <int> m1(7), m2(8);
+	ASSERT_ANY_THROW(m1 * m2);
+}
+
+TEST(TMatrix, can_divide_matrix)
+{
+	TMatrix<int> m1(2), m2(2), m(2);
+
+	m1[0][0] = 1;
+	m1[0][1] = 2;
+	m1[1][0] = 1;
+
+	m2[0][0] = 1;
+	m2[0][1] = 1;
+	m2[1][0] = 1;
+
+	m[0][0] = 1;
+	m[0][1] = 1;
+	m[1][0] = 1;
+	ASSERT_TRUE(m == (m1 / m2));
+}
+
