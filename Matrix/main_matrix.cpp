34b2a5336a7b15559799d20ac71229b91445db74
381706-1GolubevaAnna <43142260+381706-1GolubevaAnna@users.noreply.github.com>
2019-02-28 19:59:21 +0300

    Update main_matrix.cpp

--- Matrix/main_matrix.cpp
+++ Matrix/main_matrix.cpp
@@ -5,16 +5,15 @@ int main()
 {
   try
   {
-    TMatrix<int> A(2), B(2), res(2);
-    A[0][0] = 7;
-    A[0][1] = 2;
-    A[1][0] = 3;
-   
-    B[0][0] = 4;
-    B[0][1] = 3;
-    B[1][0] = 2;
- 
-   
+    TMatrix<int> A(5), B(5), res(5);
+  
+    for (i = 0; i < 5; i++)
+	  for (j = 0; j < 5 - i; j++) 
+	  {
+	  A[i][j] = i * (j + 1);
+	  B[i][j] = i * 5 + j * 4;
+	
+	  }
     res = A + B;
     cout << "A + B" << endl << res << endl;
 
