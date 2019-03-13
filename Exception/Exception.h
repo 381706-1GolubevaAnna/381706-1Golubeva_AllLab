da7371042398cea8adb67494cd8af23818d87d16
381706-1GolubevaAnna <43142260+381706-1GolubevaAnna@users.noreply.github.com>
2019-03-13 15:28:58 +0300

    Update Exception.h

--- Exception/Exception.h
+++ Exception/Exception.h
@@ -1,21 +1,16 @@
 #pragma once
-
 #include <iostream>
 #include <string>
-using namespace std;
 
 class TException
 {
-private:
-	string message;
-public:
-	TException(string _message);
-	void Print();
+  private:
+  std::string message;
+  public:
+  TException(std::string _message):message(_message) {}
+  void Print()
+  {
+  std::cout << "\nWarning! \nMessage: " << message << std::endl;
+  }
 };
 
-TException::TException(string _message) : message(_message) {}
-
-void TException::Print()
-{
-	cout << "\nWarning! \nMessage: " << message << endl;
-}
